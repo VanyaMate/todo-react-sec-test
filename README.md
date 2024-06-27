7beae0d8060581b55311ead5650b069473a57374
VanyaMate <buhu653@mail.ru>
2024-06-27 18:15:26 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todo
+
+Реализация UMA
+
+- React
+- SEC
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FC, memo } from 'react';
+
+
+export const App: FC = memo(function App () {
+    return (
+        <div></div>
+    );
+});
